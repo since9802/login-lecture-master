c79292331d173fb4f2186f26f16dc6fb163d07b4
since9802 <78550137+since9802@users.noreply.github.com>
2021-03-30 16:50:38 +0900

    login 정보와 일치하면 success

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -10,9 +10,28 @@ const output = {
   },
 };
 
+const users = {
+  id: ["since9802", "이규혁", "제발됐으면"],
+  psword: ["1234", "12345", "123456"],
+};
+
 const process = {
   login: (req, res) => {
-    console.log(req.body);
+    const id = req.body.id,
+      psword = req.body.psword;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.psword[idx] === psword) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하셨습니다.",
+    });
   },
 };
 
